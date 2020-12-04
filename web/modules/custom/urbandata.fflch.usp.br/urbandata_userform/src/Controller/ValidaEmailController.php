f62ab6d7a3c9f6095d5959598a4cb99b644b196d
Daniel MMe <medeiros.danielm@gmail.com>
2020-12-04 13:58:37 -0500

    #Urbandata project - Create custom module

---
+++ web/modules/custom/urbandata.fflch.usp.br/urbandata_userform/src/Controller/ValidaEmailController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\urbandata_userform\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+
+/**
+ * Class ValidaEmailController.
+ */
+class ValidaEmailController extends ControllerBase {
+
+  /**
+   * Drupal\Core\Database\Connection definition.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * Symfony\Component\HttpFoundation\Request;.
+   *
+   * @var Symfony\Component\HttpFoundation\Requestn
+   */
+  protected $request;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(Connection $database, RequestStack $request) {
+    $this->database = $database;
+    $this->request = $request;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database'),
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate() {
+
+
+    if (!empty($_POST['email'])) {
+      $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+      $query =  $this->database->select('webform_submission_data', 'webform');
+      $query->condition('webform.name', 'email', '=');
+      $query->condition('webform.value', $email, '=');
+      $query->fields('webform', ['sid']);
+      $sid = $query->execute()->fetchField();
+      if (!empty($sid)) {
+        $webformSubmission = \drupal::entityTypeManager()->getStorage('webform_submission')->load($sid);
+        $email = $webformSubmission->getData()['email'];
+        $session = \Drupal::request()->getSession();
+        $session->set('usuario_validado', $email);
+      }
+      else {
+        $email = false;
+      }
+    }
+    $response['retorno'] = $email;
+    return new JsonResponse( $response );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function sessao(Request $request) {
+
+    $email = false;
+    if (!empty($_POST['email'])) {
+      $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+      $session = \Drupal::request()->getSession();
+      $session->set('usuario_validado', $email);
+    }
+    $response['retorno'] = $email;
+    return new JsonResponse( $response );
+  }
+
+}
---
+++ web/modules/custom/urbandata.fflch.usp.br/urbandata_userform/src/Form/DefaultForm.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\urbandata_userform\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class DefaultForm.
+ */
+class DefaultForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'controle-acesso';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['email'] = [
+      '#type' => 'email',
+      '#weight' => '0',
+      '#required' => TRUE,
+      '#placeholder' => 'Email',
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Enviar'),
+    ];
+    $form['#theme'] = 'urbandata_template';
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    foreach ($form_state->getValues() as $key => $value) {
+      // @TODO: Validate fields.
+    }
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+
+}
